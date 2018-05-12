80cccfc88f8cb4ef091e4bb2ba782f437ee63d41
poonams21 <poonammsingh21@gmail.com>
2018-05-12 21:11:07 +0530

    commiting first maven project

---
+++ mavenfirstproject/src/main/java/com/puit/mavenfirstproject/App.java
@@ -0,0 +1,13 @@
+package com.puit.mavenfirstproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
